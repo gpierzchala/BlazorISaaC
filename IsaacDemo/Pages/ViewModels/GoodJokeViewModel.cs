0a145345d8dda4dd0d804539dadba2af8adb918a
Grzegorz Pierzchala <g_pierzchala@windowslive.com>
2019-10-31 07:57:21 +0100

    Initial checkin

---
+++ IsaacDemo/Pages/ViewModels/GoodJokeViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace IsaacDemo.Pages.ViewModels
+{
+    public class GoodJokeViewModel
+    {
+        public string Category { get; set; }
+
+        public string Type { get; set; }
+
+        public string Setup { get; set; }
+
+        public string Delivery { get; set; }
+
+        public string Joke { get; set; }
+
+        public int Id { get; set; }
+    }
+}
